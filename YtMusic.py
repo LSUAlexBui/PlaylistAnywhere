ba289ea403fa0bf38b29d42cecf6f74a2ba34ba6
LSUAlexBui <abui34@lsu.edu>
2025-08-10 21:31:26 -0500

    imported YTMusic setup

---
+++ YtMusic.py
@@ -0,0 +1,4 @@
+from ytmusticapi import YTMusic, OAuthCredentials
+ytMusic = YTMusic("browser.json")
+ytmusic - YTMusic("oauth.json", oauth_credentials=OAuthCredentials(client_id=client_id, client_secret=client_secret))
+
