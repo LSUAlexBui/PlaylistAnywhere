69cf679ae8724adc83cd3597c84a47dd93ee6ea8
Jnoble96 <Jesse.a.noble@gmail.com>
2025-08-11 10:11:29 -0500

    Update spotfiy.py

---
+++ spotfiy.py
@@ -0,0 +1,20 @@
+import requests
+
+spotifyurl = "https://api.spotify.com"
+
+client_id = "0d266fae9d834656bc19c86dabf686f8"
+client_secret = "f036bf3c228b4ce899a1d3e9089f85f2"
+
+auth_url = "https://accounts.spotify.com/api/token"
+headers = {
+    "Content-Type": "application/x-www-form-urlencoded"
+}
+data = {
+    "grant_type": "client_credentials",
+    "client_id": client_id,
+    "client_secret": client_secret
+}
+
+response = requests.post(auth_url, headers=headers, data=data)
+print(response.json())
+
