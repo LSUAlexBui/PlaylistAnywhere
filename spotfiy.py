79b401415577aabed824e8ba58fa0a356555580f
Jnoble96 <Jesse.a.noble@gmail.com>
2025-08-11 13:44:15 -0500

    Changes
    It can now recieve playlists and display song names and artists

--- spotfiy.py
+++ spotfiy.py
@@ -1,3 +1,5 @@
+import json
+from pickle import GET
 import requests
 
 spotifyurl = "https://api.spotify.com"
@@ -14,5 +16,52 @@ data = {
 }
 
 response = requests.post(auth_url, headers=headers, data=data)
-print(response.json())
+resp_json = response.json()
+print(resp_json)
 
+authorization = f"{resp_json['token_type']} {resp_json['access_token']}"
+headers = {
+    "Authorization": authorization
+}
+
+playlist_url = "https://open.spotify.com/playlist/4TULtsF069BLZAT7KG09eO?si=40a30dc6a7b04087"
+remove = "https://open.spotify.com/playlist/"
+playlist_id = playlist_url.replace(remove, "")
+api_playlist_url = "https://api.spotify.com/v1/playlists/" + playlist_id
+
+def get_playlist(url, headers):
+    playlist_response = requests.get(url, headers=headers)
+    if playlist_response.status_code != 200:
+        print(f"Error fetching playlist: {playlist_response.status_code}")
+        return
+    print(playlist_response.json())
+    json_data1 = playlist_response.json()
+    return json_data1
+json_data1 = get_playlist(api_playlist_url, headers)
+artist = []
+songName = []
+
+def extract_artists_and_songs(json_data):
+    if not json_data or 'tracks' not in json_data or 'items' not in json_data['tracks']:
+        print("No tracks/items found in playlist data.")
+        return
+    for item in json_data['tracks']['items']:
+        # Get the track info
+        track = item.get('track')
+        if not track:
+            continue
+        # Get the song name
+        name = track.get('name')
+        if name:
+            songName.append(name)
+        # Get the first artist name
+        artists_list = track.get('artists')
+        if artists_list and len(artists_list) > 0:
+            artist_name = artists_list[0].get('name')
+            if artist_name:
+                artist.append(artist_name)
+
+extract_artists_and_songs(json_data1)
+
+print("Artists:", artist)
+print("Songs:", songName)
