3c3e4dd2d1bbd9a1a3720add5cab586490ecd06f
Jnoble96 <Jesse.a.noble@gmail.com>
2025-09-01 14:19:49 -0500

    Update spotfiy.py

--- spotfiy.py
+++ spotfiy.py
@@ -22,46 +22,84 @@ headers = {
     "Authorization": authorization
 }
 
-playlist_url = "https://open.spotify.com/playlist/1sfIs6G2KpBwueBLVzEmWr?si=858b887bb6db41fc"
+playlist_url = "https://open.spotify.com/playlist/47ijO8ixCB6ZZGMId8JHPY?si=63de8bff12164177"
 remove = "https://open.spotify.com/playlist/"
 playlist_id = playlist_url.replace(remove, "")
 api_playlist_url = "https://api.spotify.com/v1/playlists/" + playlist_id
 
-def get_playlist(url, headers):
-    playlist_response = requests.get(url, headers=headers)
-    if playlist_response.status_code != 200:
-        print(f"Error fetching playlist: {playlist_response.status_code}")
-        return
-    print(playlist_response.json())
-    json_data1 = playlist_response.json()
-    return json_data1
-json_data1 = get_playlist(api_playlist_url, headers)
-artist = []
-songName = []
+playlist = {
+        "title": "",
+        "coverArt": {
+            "url": "",
+            "width": 640,
+            "height": 640
+        },
+        "description": "",
+        "tracks": [],
+        "artists": []       
+    }
 
-def extract_artists_and_songs(json_data):
-    if not json_data or 'tracks' not in json_data or 'items' not in json_data['tracks']:
-        print("No tracks/items found in playlist data.")
-        return
-    for item in json_data['tracks']['items']:
-        track = item.get('track')
-        if not track:
-            continue
-        name = track.get('name')
-        if name:
-            songName.append(name)
-        artists_list = track.get('artists')
-        if artists_list and len(artists_list) > 0:
-            artist_name = artists_list[0].get('name')
-            if artist_name:
-                artist.append(artist_name)
 
-extract_artists_and_songs(json_data1)
 
-print("Artists:", artist)
-print("Songs:", songName)
+class SpotifyMusic:
+    
+    playlist = {
+        "title": "",
+        "coverArt": {
+            "url": "",
+            "width": 640,
+            "height": 640
+        },
+        "description": "",
+        "tracks": [],
+        "artists": []       
+    }
+    def get_playlist(url, headers):
+        playlist_response = requests.get(url, headers=headers)
+        if playlist_response.status_code != 200:
+            print(f"Error fetching playlist: {playlist_response.status_code}")
+            return
+        print(playlist_response.json())
+        json_data1 = playlist_response.json()
+        return json_data1
+    json_data1 = get_playlist(api_playlist_url, headers)
 
-def login(client_id, response_type, redirect_uri):
-    login_url = f"https://accounts.spotify.com/authorize?client_id={client_id}&response_type={response_type}&redirect_uri={redirect_uri}"
-    return login_url
-print("Login URL:", login(client_id, response_type, redirect_uri))
+    def extract_artists_and_songs(json_data):
+        if not json_data or 'tracks' not in json_data or 'items' not in json_data['tracks']:
+            print("No tracks/items found in playlist data.")
+            return
+        for item in json_data['tracks']['items']:
+            track = item.get('track')
+            if not track:
+                continue
+            name = track.get('name')
+            if name:
+                SpotifyMusic.playlist["tracks"].append(name)
+            artists_list = track.get('artists')
+            if artists_list and len(artists_list) > 0:
+                artist_name = artists_list[0].get('name')
+                if artist_name:
+                    SpotifyMusic.playlist["artists"].append(artist_name)
+        SpotifyMusic.playlist["coverArt"]["url"] = ""
+        images = json_data.get("images", [])
+        SpotifyMusic.playlist["coverArt"]["url"] = images[0]["url"]
+        SpotifyMusic.playlist["title"] = json_data.get("name", "")
+        SpotifyMusic.playlist["description"] = json_data.get("description", "")
+    def createPlaylist(self.playlistdata =()):
+
+
+
+        
+                    
+    
+    
+
+    def login(client_id, response_type, redirect_uri):
+        login_url = f"https://accounts.spotify.com/authorize?client_id={client_id}&response_type={response_type}&redirect_uri={redirect_uri}"
+        return login_url
+    print("Login URL:", login(client_id, response_type, redirect_uri))
+SpotifyMusic.extract_artists_and_songs(SpotifyMusic.json_data1)
+print("Artists:", SpotifyMusic.playlist["artists"])
+print("Songs:", SpotifyMusic.playlist["tracks"])
+print(SpotifyMusic.playlist["coverArt"]["url"])
+print(SpotifyMusic.playlist["description"])
