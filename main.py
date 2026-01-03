11848162ea8681f46cb67f4882f1a84d82d86ff7
LSUAlexBui <alexdbui4@gmail.com>
2026-01-02 23:00:08 -0600

    Actually changed project layout

--- main.py
+++ main.py
@@ -1,31 +1,10 @@
 import requests
 from flask import Flask, request, jsonify, render_template
-from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.sqlite3'
-db = SQLAlchemy(app)
-
-
-
-class user_id(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    spotify_linked = db.Column(db.Boolean,default = False)
-    YTmusic_linked = db.Column(db.Boolean, default =False)
-    
-    def __repr__(self):
-        return '<User %r>' % self.id
-
 
 @app.route("/")
 def home():
-    return render_template('index.html')
-
-if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True) 
-    app.run(port=5500)
 
 @app.route("/callback", methods=["POST", "GET"])
 def callback():
